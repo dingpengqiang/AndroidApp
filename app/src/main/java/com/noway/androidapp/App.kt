868a869914ddf56973bb1ef03b84e07b98af270c
dingpengqiang <dingpengqiang>
2018-03-21 10:06:28 +0800

    算法面试题

---
+++ app/src/main/java/com/noway/androidapp/App.kt
@@ -0,0 +1,22 @@
+package com.noway.androidapp
+
+import android.app.Application
+
+
+/**
+ * 包名:    com.noway.androidapp
+ * 标题:
+ * 描述:    TODO
+ * 作者:    NoWay
+ * 邮箱:    dingpengqiang@qq.com
+ * 日期:    2018/3/16
+ * 版本:    V-1.0.0
+ */
+class App :Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
