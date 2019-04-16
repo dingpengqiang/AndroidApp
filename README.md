27a296eeaf2e1c4a283ea0d157b30fff40f53c90
dingpq <dingpq@zjmax.com>
2019-04-16 13:38:09 +0800

    抽屉DrawerLayout + NavigationView的使用

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AndroidApp
 系统的学习Android的知识库
+
+
+知识库：
+    1.学习资料
+    2.开发规范
+    3.面试总结
+
+
+DrawerLayout 的简单使用
+DrawerLayout +
---
+++ drawerlayout_navigationview/src/main/java/com/noway723/drawerlayout_nav/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.noway723.drawerlayout_nav
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
