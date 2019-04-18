874f783354c5922f3a7ef010cae8fbe0a521ac1e
dingpq <dingpq@zjmax.com>
2019-04-18 09:39:38 +0800

    Palette取色器的使用

---
+++ jetpack_palette/src/main/java/com/noway723/jetpack_palette/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.noway723.jetpack_palette
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ *  Android Jetpack:  Palette(调色板) 的基本用法
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
